57329ef5ee4ccb08964af7eb5b51b26bcfd8ba07
Victor Caio <kaioaraujo426@gmail.com>
2024-06-05 00:05:06 -0300

    FEAT: continuation of the change in responsiveness on smaller screens of the create page

--- src/views/CreateUser.tsx
+++ src/views/CreateUser.tsx
@@ -114,114 +114,120 @@ export default function CreateUser() {
         justifyContent: 'center',
       }}
     >
-      <Card
+      {/* <Card
         style={{
-          // width: '380px',
-          margin: '0 auto',
+          width: screens.xs ? undefined : '380px',
+          // margin: '0 auto',
+          border: '1px solid #ccc',
+          borderRadius: '10px',
+          // height: '80vh',
+          boxShadow: '5px 8px 24px 5px rgba(208, 216, 243, 0.6)',
+        }}
+      > */}
+      <section
+        style={{
+          alignItems: 'center',
+          backgroundColor: '#fff',
+          display: 'flex',
           border: '1px solid #ccc',
           borderRadius: '10px',
           boxShadow: '5px 8px 24px 5px rgba(208, 216, 243, 0.6)',
+          // height: screens.sm ? '90vh' : 'auto',
+          // height: 'auto',
+
+          padding: screens.md ? `${token.sizeXXL}px 0px` : '0px',
         }}
       >
-        <section
+        <div
           style={{
-            alignItems: 'center',
-            backgroundColor: token.colorBgContainer,
-            display: 'flex',
-            // height: screens.sm ? '90vh' : 'auto',
-            height: 'auto',
-            padding: screens.md ? `${token.sizeXXL}px 0px` : '0px',
+            margin: '0 auto',
+            padding: screens.md
+              ? `${token.paddingXL}px`
+              : `${token.sizeXXL}px ${token.padding}px`,
+            width: screens.xs ? '300px' : '380px',
           }}
         >
           <div
             style={{
-              margin: '0 auto',
-              padding: screens.md
-                ? `${token.paddingXL}px`
-                : `${token.sizeXXL}px ${token.padding}px`,
-              // width: '380px',
+              marginBottom: token.marginSM,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
             }}
           >
-            <div
-              style={{
-                marginBottom: token.marginXL,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
-            >
-              <Image
-                src={'/logo.png'}
-                alt="Logo da empresa"
-                width={60}
-                height={60}
-              />
+            <Image
+              src={'/logo.png'}
+              alt="Logo da empresa"
+              width={60}
+              height={60}
+              loading="lazy"
+            />
 
-              <Title style={styles.title}>Criar conta</Title>
-              {/* <Text style={styles.text}>
+            <Title style={styles.title}>Criar conta</Title>
+            {/* <Text style={styles.text}>
               Welcome back to AntBlocks UI! Please enter your details below to
               sign in.
             </Text> */}
-            </div>
-            <Form
-              name="normal_login"
-              initialValues={{
-                remember: true,
-              }}
-              onFinish={onFinish}
-              layout="vertical"
-              requiredMark="optional"
-              // action={action}
+          </div>
+          <Form
+            name="normal_login"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            layout="vertical"
+            requiredMark="optional"
+            // action={action}
+          >
+            <Form.Item
+              name="name"
+              label="Nome"
+              rules={[
+                {
+                  type: 'string',
+                  required: true,
+                  message: 'Please input your name!',
+                },
+              ]}
             >
-              <Form.Item
-                name="name"
-                label="Nome"
-                rules={[
-                  {
-                    type: 'string',
-                    required: true,
-                    message: 'Please input your name!',
-                  },
-                ]}
-              >
-                <Input
-                  prefix={<UserOutlined />}
-                  placeholder="Digite seu nome completo"
-                />
-              </Form.Item>
-              <Form.Item
-                name="email"
-                label="Email"
-                rules={[
-                  {
-                    type: 'email',
-                    required: true,
-                    message: 'Please input your Email!',
-                  },
-                ]}
-              >
-                <Input
-                  prefix={<MailOutlined />}
-                  placeholder="Digite seu melhor e-mail"
-                />
-              </Form.Item>
-              <Form.Item
-                label="Senha"
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Please input your Password!',
-                  },
-                ]}
-              >
-                <Input.Password
-                  prefix={<LockOutlined />}
-                  type="password"
-                  placeholder="Digite sua senha"
-                />
-              </Form.Item>
-              {/* <Form.Item>
+              <Input
+                prefix={<UserOutlined />}
+                placeholder="Digite seu nome completo"
+              />
+            </Form.Item>
+            <Form.Item
+              name="email"
+              label="Email"
+              rules={[
+                {
+                  type: 'email',
+                  required: true,
+                  message: 'Please input your Email!',
+                },
+              ]}
+            >
+              <Input
+                prefix={<MailOutlined />}
+                placeholder="Digite seu melhor e-mail"
+              />
+            </Form.Item>
+            <Form.Item
+              label="Senha"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Password!',
+                },
+              ]}
+            >
+              <Input.Password
+                prefix={<LockOutlined />}
+                type="password"
+                placeholder="Digite sua senha"
+              />
+            </Form.Item>
+            {/* <Form.Item>
                 <Form.Item name="remember" valuePropName="checked" noStyle>
                 <Checkbox>Remember me</Checkbox>
               </Form.Item>
@@ -229,30 +235,25 @@ export default function CreateUser() {
                   Esqueceu a senha?
                 </a>
               </Form.Item> */}
-              <Form.Item style={{ marginBottom: '0px' }}>
-                <Button
-                  block
-                  type="primary"
-                  htmlType="submit"
-                  loading={loading}
-                >
-                  Criar
-                </Button>
-                <div
-                  style={{
-                    marginTop: token.marginLG,
-                    textAlign: 'center',
-                    width: '100%',
-                  }}
-                >
-                  {/* <Text style={styles.text}>Não tem uma conta? </Text>{' '}
+            <Form.Item style={{ marginBottom: '0px' }}>
+              <Button block type="primary" htmlType="submit" loading={loading}>
+                Criar
+              </Button>
+              <div
+                style={{
+                  marginTop: token.marginLG,
+                  textAlign: 'center',
+                  width: '100%',
+                }}
+              >
+                {/* <Text style={styles.text}>Não tem uma conta? </Text>{' '}
                   <Link href="">Inscreva-se agora</Link> */}
-                </div>
-              </Form.Item>
-            </Form>
-          </div>
-        </section>
-      </Card>
+              </div>
+            </Form.Item>
+          </Form>
+        </div>
+      </section>
+      {/* </Card> */}
     </div>
   );
 }
