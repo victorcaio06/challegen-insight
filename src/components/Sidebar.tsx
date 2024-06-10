11523cd9dd2ecb0d482901f9427316c6ec0c6f75
Victor Caio <kaioaraujo426@gmail.com>
2024-06-09 22:49:22 -0300

    REFACTOR: simplify sidebar menu and remove unnecessary code

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -31,54 +31,7 @@ export default function Sidebar() {
         router.push('/fornecedores');
       },
     },
-    {
-      label: 'Produtos/Serviços',
-      key: 'app',
-      icon: <ProductOutlined />,
-      onClick: () => {
-        message.error('Página não implementada!');
-      },
-    },
-    // {
-    //   label: 'Navigation Three - Submenu',
-    //   key: 'SubMenu',
-    //   icon: <FileOutlined />,
-    //   children: [
-    //     {
-    //       type: 'group',
-    //       label: 'Item 1',
-    //       children: [
-    //         { label: 'Option 1', key: 'setting:1' },
-    //         { label: 'Option 2', key: 'setting:2' },
-    //       ],
-    //     },
-    //     {
-    //       type: 'group',
-    //       label: 'Item 2',
-    //       children: [
-    //         { label: 'Option 3', key: 'setting:3' },
-    //         { label: 'Option 4', key: 'setting:4' },
-    //       ],
-    //     },
-    //   ],
-    // },
-    // {
-    //   key: 'alipay',
-    //   label: (
-    //     <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-    //       Navigation Four - Link
-    //     </a>
-    //   ),
-    // },
   ];
-
-  function mobileTriggerStyle(isMobile: boolean) {
-    if (isMobile) {
-      return { display: 'block' };
-    } else {
-      return { display: 'none' };
-    }
-  }
 
   function handleResize() {
     setScreenSize(window.innerWidth);
@@ -91,36 +44,17 @@ export default function Sidebar() {
     };
   }, []);
 
-  // Rest of the component code
   return (
     <Sider
       collapsible
       collapsed={collapsed}
       onCollapse={(value) => setCollapsed(value)}
       breakpoint="lg"
-      // collapsedWidth="0"
       collapsedWidth={screenSize >= 768 ? undefined : 0}
-      onBreakpoint={(broken) => {
-        console.log(broken);
-      }}
       zeroWidthTriggerStyle={{
-        // height: '48px',
         marginBottom: '8px',
         marginTop: '-50px',
-        // marginLeft: '8px',
-        // marginRight: '8px',
-        // backgroundColor: 'transparent',
       }}
-      // className="flex flex-col items-start justify-evenly"
-      // style={{
-      //   flex: 1,
-      //   display: 'flex',
-      //   flexDirection: 'column',
-      //   justifyContent: 'space-between',
-      //   justifyItems: 'stretch',
-      //   alignItems: 'stretch',
-      //   backgroundColor: 'red',
-      // }}
     >
       <div className="flex flex-1 items-center justify-center mt-2">
         <Image src="/logo.png" alt="logo" width={40} height={40} />
