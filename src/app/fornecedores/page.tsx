6367d0a6b563b52784b85a8175267b342d3a6c9a
Victor Caio <kaioaraujo426@gmail.com>
2024-06-09 22:47:08 -0300

    FEAT: add dynamic supplier list fetching and loading state

--- src/app/fornecedores/page.tsx
+++ src/app/fornecedores/page.tsx
@@ -1,11 +1,58 @@
 import PageWithoutContent from '@/components/PageWithoutContent';
-import SupplierRegistration from '@/views/SupplierRegistration';
+import SuppliersListHeader from '@/components/SuppliersListHeader';
+import { SupplierData } from '@/utils/supplierDataTypes';
+import { Spin } from 'antd';
+import dynamic from 'next/dynamic';
+
+const ListSuppliers = dynamic(() => import('@/views/ListSuppliers'), {
+  ssr: false,
+});
+
+let loading = true;
+
+async function getListSuppliers() {
+  // TODO: Colocar em um try catch
+  const response = await fetch('http://localhost:3000/api/suppliers', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    cache: 'no-store',
+  });
+
+  if (!response.ok) {
+    return null;
+  }
+
+  const data = await response.json();
+
+  const dataType = data.data as SupplierData[];
+
+  loading = false;
+  return { data: dataType };
+}
 
 export default async function SupplierPage() {
+  const data = await getListSuppliers();
+
+  if (!data || data.data.length === 0) {
+    return (
+      <PageWithoutContent
+        message="Nenhum fornecedor encontrado!"
+        to="/fornecedores/create"
+      />
+    );
+  }
+
   return (
-    <PageWithoutContent
-      message="Nenhum fornecedor encontrado!"
-      to="/fornecedores/create"
-    />
+    <>
+      <SuppliersListHeader />
+
+      {loading ? (
+        <Spin size="default" style={{ margin: 'auto' }} />
+      ) : (
+        <ListSuppliers data={data.data} />
+      )}
+    </>
   );
 }
